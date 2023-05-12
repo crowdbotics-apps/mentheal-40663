b5ed61ccb65b8c56674a6ca44fb65112e4904d10
Crowdbotics <team@crowdbotics.com>
2023-05-12 21:13:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mentheal-40663",
+  url: "https://mentheal-40663.botics.co",
+  api: "https://mentheal-40663.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
